2cb4f6c6ddba247728dd6ea4fb58c09a3cf2537b
dima888 <foxhound@ws-81-217.haw-1x.haw-hamburg.de>
2014-11-19 08:20:58 +0100

    Sync einfach

--- Praktikum/out/production/Praktikum/graphviz.erl
+++ Praktikum/out/production/Praktikum/graphviz.erl
@@ -1,3 +1,5 @@
+%% Quelle: https://github.com/glejeune/erlang-graphviz/blob/master/src/graphviz.erl
+
 -module(graphviz).
 -export([digraph/1, graph/1, delete/0, add_node/1, add_edge/2, graph_server/1, to_dot/1, to_file/2]).
 
